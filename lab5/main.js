bf7fa058a1676d02855c5257f0553bea59e17f90
Catalina <catalina@Catalinas-MacBook-Pro.local>
2024-05-07 16:10:01 +0200

    addserach

--- lab5/main.js
+++ lab5/main.js
@@ -1,3 +1,4 @@
+
 // Definition Rijksdriehoekstelsel (EPSG:28992)
 let res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105];
 let map = L.map('map-canvas', {
@@ -29,3 +30,35 @@ let baseLayers = {
   "Topographical map": basemap_pdok
 };
 let toc = L.control.layers(baseLayers).addTo(map);
+
+// Register a geocoder to the map app 
+register_geocoder = function (mapInstance) {
+  let polygon = null;
+
+  function clear() {
+    if (polygon !== null) {
+      mapInstance.removeLayer(polygon);
+    }
+  }
+
+  var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+  })
+    .on('markgeocode', function (e) {
+      clear()
+      var bbox = e.geocode.bbox;
+      polygon = L.polygon([
+        bbox.getSouthEast(),
+        bbox.getNorthEast(),
+        bbox.getNorthWest(),
+        bbox.getSouthWest()
+      ]);
+      mapInstance.addLayer(polygon);
+      mapInstance.fitBounds(polygon.getBounds());
+      setTimeout(clear, 2500);
+    })
+    .addTo(mapInstance);
+  return geocoder;
+}
+
+register_geocoder(map)
